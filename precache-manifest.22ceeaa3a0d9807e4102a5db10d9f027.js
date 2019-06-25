551fc0599efe0911c7ffb4a1b0b362d5134bbe17
Dan Schultz <travelguy90@gmail.com>
2019-06-25 07:10:42 -0700

    Updates

---
+++ precache-manifest.22ceeaa3a0d9807e4102a5db10d9f027.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c08ba2b47ff638596a83dbeb38e292fc",
+    "url": "/react-best-selling-books/index.html"
+  },
+  {
+    "revision": "040c53bf5b22043fa83c",
+    "url": "/react-best-selling-books/static/css/2.c7bb94bc.chunk.css"
+  },
+  {
+    "revision": "fbd29dc8210c4ef77b49",
+    "url": "/react-best-selling-books/static/css/main.f43c046f.chunk.css"
+  },
+  {
+    "revision": "040c53bf5b22043fa83c",
+    "url": "/react-best-selling-books/static/js/2.3a286381.chunk.js"
+  },
+  {
+    "revision": "fbd29dc8210c4ef77b49",
+    "url": "/react-best-selling-books/static/js/main.3ce44a90.chunk.js"
+  },
+  {
+    "revision": "7cf1bc6300122f6133c1",
+    "url": "/react-best-selling-books/static/js/runtime~main.422d7087.js"
+  }
+]);
