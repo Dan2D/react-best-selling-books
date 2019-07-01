4f10d036478cf8c36927598d6fe1ab3e4459ef53
Dan Schultz <travelguy90@gmail.com>
2019-07-01 10:24:13 -0700

    Updates

--- precache-manifest.ed94031f6f10d6b985547d6aecb9c4ad.js
+++ precache-manifest.ed94031f6f10d6b985547d6aecb9c4ad.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2e6f6b41433f93654c18042b9d42bd19",
+    "revision": "e3cb006b96f6b43d73a877acf255bb1f",
     "url": "/react-best-selling-books/index.html"
   },
   {
@@ -8,15 +8,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-best-selling-books/static/css/2.c7bb94bc.chunk.css"
   },
   {
-    "revision": "d385a3d73b171ce3245b",
-    "url": "/react-best-selling-books/static/css/main.75aa677d.chunk.css"
+    "revision": "8c5f96f7ebd7a3932877",
+    "url": "/react-best-selling-books/static/css/main.95029c9f.chunk.css"
   },
   {
     "revision": "f9c7f5099b73263d40c9",
     "url": "/react-best-selling-books/static/js/2.bbafa56c.chunk.js"
   },
   {
-    "revision": "d385a3d73b171ce3245b",
+    "revision": "8c5f96f7ebd7a3932877",
     "url": "/react-best-selling-books/static/js/main.6cba44f7.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-best-selling-books/precache-manifest.d7e8a5c172ea77085fb8257055afb0c2.js"
+  "/react-best-selling-books/precache-manifest.ed94031f6f10d6b985547d6aecb9c4ad.js"
 );
 
 self.addEventListener('message', (event) => {
