77073cc78fa6ce93d656909a985f48919bf1f3c7
Dan Schultz <travelguy90@gmail.com>
2019-08-09 09:10:28 -0700

    Updates

---
+++ precache-manifest.e362fae992aea332a0be77449dae0b23.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f80a318f71a23d97e9cf0b8bf254682a",
+    "url": "/react-best-selling-books/index.html"
+  },
+  {
+    "revision": "826461f9971c73217dbd",
+    "url": "/react-best-selling-books/static/css/2.c7bb94bc.chunk.css"
+  },
+  {
+    "revision": "2ba57356238e3f91e2fe",
+    "url": "/react-best-selling-books/static/css/main.2431bd37.chunk.css"
+  },
+  {
+    "revision": "826461f9971c73217dbd",
+    "url": "/react-best-selling-books/static/js/2.b3bd080c.chunk.js"
+  },
+  {
+    "revision": "768e00e01ae4dc603fd4",
+    "url": "/react-best-selling-books/static/js/3.a5878e9b.chunk.js"
+  },
+  {
+    "revision": "c8b13ed99e87c9b87211",
+    "url": "/react-best-selling-books/static/js/4.6e7f5c80.chunk.js"
+  },
+  {
+    "revision": "55cd3fd63e53708854ed",
+    "url": "/react-best-selling-books/static/js/5.96d9815c.chunk.js"
+  },
+  {
+    "revision": "2ba57356238e3f91e2fe",
+    "url": "/react-best-selling-books/static/js/main.adc4d38b.chunk.js"
+  },
+  {
+    "revision": "d32bc1b7ea9dc694ba60",
+    "url": "/react-best-selling-books/static/js/runtime~main.a7e0618b.js"
+  },
+  {
+    "revision": "ab11e70ca52caa9aefdadf0c021ba5b9",
+    "url": "/react-best-selling-books/static/media/404.ab11e70c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://dan2d.github.io/react-best-selling-books/precache-manifest.90f257f55d93c98edcb328775b715a3b.js"
+  "/react-best-selling-books/precache-manifest.e362fae992aea332a0be77449dae0b23.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://dan2d.github.io/react-best-selling-books/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-best-selling-books/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
